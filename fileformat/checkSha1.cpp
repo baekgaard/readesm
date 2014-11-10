188d9f1d974361734f7c7b478d54925c89efcea7
Per Baekgaard <baekgaard@b4net.dk>
2014-11-10 17:37:16 +0100

    Minor change in checksum checking to fix an error (presumably)

--- fileformat/checkSha1.cpp
+++ fileformat/checkSha1.cpp
@@ -7,6 +7,7 @@
 
 bool checkSha1(const RawData& data, const QByteArray& hash) {
 	QByteArray buffer(20, 0);
+	if (hash.size() < 20) return false;
 	gcry_md_hash_buffer(GCRY_MD_SHA1, buffer.data(), data.toUnsignedPointer(), data.size());
 	for(int j = 0; j < 20; ++j) {
 		if(hash[j] != buffer[j]) return false;
